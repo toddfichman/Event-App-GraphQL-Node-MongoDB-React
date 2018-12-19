e170b614a75c4e0222e9887acada2d480868a365
Todd Fichman <toddfichman@gmail.com>
2018-12-19 10:56:47 -0500

    Created a user schema and resolver for graphql

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ const graphQlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -35,11 +37,22 @@ app.use('/graphql', graphQlHttp({
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
         }
 
         type RootQuery {
@@ -48,6 +61,7 @@ app.use('/graphql', graphQlHttp({
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User 
         }
 
         schema {
@@ -94,6 +108,31 @@ app.use('/graphql', graphQlHttp({
                     console.log(err, 'failed post to DB');
                     throw err;
                 })
+        },
+        createUser: (args) => {
+            return User.findOne({email: args.userInput.email}) //Checking user DB to see if email exists already
+                .then(user => {
+                    if (user) { //if the user exists 
+                        throw new Error('User already exists')
+                    } // else create user
+                    //THIS HASHES USER PASSWORD
+                    return bcrypt.hash(args.userInput.password, 12) //1st arg = what to hash, 2nd arg = length of salt used to hash
+                }) 
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: hashedPassword //stores hashed password in DB
+                    });
+                    return user.save(); //saves user to DB
+                })
+                .then(result => {
+                     return { ... result._doc, password: null, _id: result.id }  //set password to null so it isnt returned     
+                })
+                .catch(err => {
+                    throw err
+                })
+                
+
         }
     },
     graphiql: true
